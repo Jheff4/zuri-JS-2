10e4471c7ec9946dd22988c43b8465661d2452a3
Jheff4 <75406244+Jheff4@users.noreply.github.com>
2022-06-22 23:35:57 +0100

    initial push

---
+++ index.js
@@ -0,0 +1,11 @@
+const name = 'Etinosa';
+const height = '1.75 metres';
+const country = 'Nigeria';
+
+
+const first = document.getElementById('container')
+
+first.innerHTML =
+  `<h2>My name is ${name}</h2>
+  <h2>I'm ${height} tall</h2>
+  <h2>The name of my country is ${country}</h2>`
